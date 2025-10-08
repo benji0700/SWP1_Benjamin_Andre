01075fef58cfa0867694db22a392292bdd5789d3
benji0700 <benji1807@outlook.com>
2025-10-08 16:50:14 +0200

    Lott überarbeitet und Hausübung fertig

--- Projekte/02.Lottoziehung.py
+++ Projekte/02.Lottoziehung.py
@@ -1,21 +1,27 @@
 import random
 
-vorkommen = {}
+def initialisiere_vorkommen():
+    vorkommen = {}
+    for i in range(1, 46):
+        vorkommen[i] = 0
+    return vorkommen
 
-for i in range(1, 46):
-    vorkommen[i] = 0
-
-for i in range(1000):
+def ziehung(vorkommen):
     zahl = list(range(1, 46))
     for j in range(6):
         gezogen = random.randint(0, len(zahl) - j - 1)
         gezogene_zahl = zahl[gezogen]
-
         vorkommen[gezogene_zahl] += 1
         letzte_position = len(zahl) - j - 1
         letzte_zahl = zahl[letzte_position]
-
         zahl[gezogen] = letzte_zahl
         zahl[letzte_position] = gezogene_zahl
 
-print("Ergebnis:\n" + str(vorkommen))
+def main():
+    vorkommen = initialisiere_vorkommen()
+    for i in range(100000):
+        ziehung(vorkommen)
+    print("Ergebnis:\n" + str(vorkommen))
+
+if __name__ == "__main__":
+    main()
